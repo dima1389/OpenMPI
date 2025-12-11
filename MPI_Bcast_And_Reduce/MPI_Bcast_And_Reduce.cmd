2d30a7e0adab96f0aa13129445248689f382b0c6
dima1389 <dimitrije.cuk@gmail.com>
2025-12-11 23:40:34 +0100

    Refactor MPI build tasks and update environment paths

--- MPI_Bcast_And_Reduce/MPI_Bcast_And_Reduce.cmd
+++ MPI_Bcast_And_Reduce/MPI_Bcast_And_Reduce.cmd
@@ -2,7 +2,7 @@
 setlocal
 
 rem --- Make sure MinGW’s DLLs come first so there is no crc32_combine popup ---
-set "PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%SystemRoot%\system32"
+set "PATH=C:\msys64\mingw64\bin;C:\msys64\usr\bin;%SystemRoot%\system32;%PATH%"
 
 rem --- Define clean MSMPI paths WITHOUT trailing backslash ---
 set "MSMPI_INC=C:\Program Files (x86)\Microsoft SDKs\MPI\Include"
@@ -11,6 +11,7 @@ set "MSMPI_LIB64=C:\Program Files (x86)\Microsoft SDKs\MPI\Lib\x64"
 cd /d %~dp0
 
 rem --- Build MPI program with MinGW g++ + MSMPI ---
+echo Building MPI_Bcast_And_Reduce...
 g++ MPI_Bcast_And_Reduce.c ^
   -I"%MSMPI_INC%" ^
   -L"%MSMPI_LIB64%" ^
